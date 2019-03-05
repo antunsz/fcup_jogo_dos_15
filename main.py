06c29f2234229b1182a8fd395207c329d04edd8c
Carlos <c4rlo4@gmail.com>
2019-03-05 20:20:26 +0000

    Criação de Matrix e movimentos - OK

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import Checker, Puzzle
+from utils import Checker, Puzzle, Board
 
 
 initial_config = [1,2,3,4,5,6,8,12,13,9,0,7,14,11,10,15]
@@ -6,6 +6,7 @@ final_config =   [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
 
 puzzle = Puzzle(4, initial_config, final_config)
 checker = Checker(puzzle)
+
 
 def init_game():
     if checker.there_is_no_solution():
@@ -21,3 +22,9 @@ if __name__ == "__main__":
     print(checker.puzzle.points)
     print(puzzle.number_out_of_place())
     puzzle.print_tree(puzzle.final_tree)
+    print(puzzle.initial_board)
+    print(puzzle.initial_board.board)
+    print("mover para cima")
+    puzzle.initial_board.move_up()
+    print(puzzle.initial_board)
+    #print(puzzle.final_board)
